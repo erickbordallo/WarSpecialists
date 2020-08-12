1f8727abb1bb37dfdfd83752ae0c503b6161573e
ScottXileChen <53885585+ScottXileChen@users.noreply.github.com>
2020-08-12 11:56:04 +0800

    Add moving method and isBlocked method in player movement scripts.

---
+++ WarSpecialists/Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed;
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Moving(Input.mousePosition);
+        }
+    }
+
+    private void Moving(Vector3 mousePosition)
+    {
+        if (!IsBlocked(mousePosition))
+        {
+            // Moving
+        }
+    }
+
+    private bool IsBlocked(Vector3 mousePosition)
+    {
+        // Check isblocked
+        return true;
+    }
+}
