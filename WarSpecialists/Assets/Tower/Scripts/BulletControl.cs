f27b5ab9b9c96374072f7f9ba478178e6e8d3217
zhanhaor <zhanhaor05@163.com>
2020-08-25 20:43:52 -0700

    Changing of the small tower controller and the tower attack scripts

---
+++ WarSpecialists/Assets/Tower/Scripts/BulletControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+    public Transform target;
+    
+    [SerializeField]
+    private float SpeedOfBullet = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * Time.deltaTime*SpeedOfBullet);
+
+    }
+
+    
+}
