3961a030f21a665c642f76a011c87a457de4c939
ScottXileChen <53885585+ScottXileChen@users.noreply.github.com>
2020-09-01 21:56:22 +0800

    Added attack animation. Update PlayerMovement, MariaAC scripts. Added Targetable, InputTarget, and HeroCombat scripts.

--- WarSpecialists/Assets/Player/Scripts/PlayerBase.cs
+++ WarSpecialists/Assets/Player/Scripts/PlayerBase.cs
@@ -7,6 +7,8 @@ public abstract class PlayerBase : MonoBehaviour
     private int _health;
     [SerializeField]
     private float _moveSpeed;
+    [SerializeField]
+    private float _AttackSpeed;
     public bool IsAlive { get { return Health > 0; } }
     public int Health { get => _health; set => _health = value; }
     public int Attack { get; protected set; }
@@ -14,8 +16,8 @@ public abstract class PlayerBase : MonoBehaviour
     public int Deffense { get; protected set; }
     public int SpecialtyPoints { get; protected set; }
     public int Gold { get; protected set; }
-    public float AttackSpeed { get; protected set; }
     public float MoveSpeed { get => _moveSpeed; set => _moveSpeed = value; }
+    public float AttackSpeed { get => _AttackSpeed; set => _AttackSpeed = value; }
 
     virtual protected void Start() { }
     virtual protected void Update() { }
