e6612dd75e772f0fc09fc8f959b03f62323e8103
g18640383061 <g18640383061@gmail.com>
2020-09-14 21:18:01 -0700

    make healthbar for minion and tower

--- WarSpecialists/Assets/Minion/Scripts/Minion.cs
+++ WarSpecialists/Assets/Minion/Scripts/Minion.cs
@@ -1,9 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Minion : MonoBehaviour
 {
+    private GameTypes.Lane mLane;
+    private float mHealth = 100.0f;
+    private float mDamage = 5.0f;
+
+    public HealthBar mHealthBar;
+
+    void Start()
+    {
+       // mHealthBar = GameObject.FindGameObjectWithTag("canvas_minion_health").GetComponent<HealthBar>();
+        mHealthBar.setMaxHealth(mHealth);
+    }
+
+     void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20.0f);
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            transform.Rotate(0, -60 * Time.deltaTime * 5, 0, Space.Self);
+        }
+
+    }
     public GameTypes.Lane lane
     {
         get { return mLane; }
@@ -16,13 +42,12 @@ public class Minion : MonoBehaviour
     public void TakeDamage(float _damage)
     {
         mHealth -= _damage;
+        mHealthBar.setHealth(mHealth);
         if(mHealth <= 0.0f)
         {
             Destroy(gameObject);
         }
     }
 
-    private GameTypes.Lane mLane;
-    private float mHealth = 100.0f;
-    private float mDamage = 5.0f;
+    
 }
---
+++ WarSpecialists/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider mSlider;
+    public void setMaxHealth(float health)
+    {
+        mSlider.maxValue = health;
+        mSlider.value = health;
+    }
+
+    public void setHealth(float health)
+    {
+        mSlider.value = health;
+    }
+}
